94bb16af5aecdc2be2114f3ccce9657865bcb32b
AbramovAleksander <uuser@bk.ru>
2023-07-19 10:41:17 +0600

    feat

---
+++ scripts/apps.js
@@ -0,0 +1,14 @@
+// number 1 2 3 4 5 6 7 8 9,  1.5 
+// string "cat" 'cat' `cat`
+// boolean true false
+// object {name: 'Dima'} - object    [] - array
+// symbol {"name": 'Dima', name: Dima}
+// undefined - nothing
+// null - 
+// BigInt
+// + - / * ** % ++ -- += -= typeof ()
+// = == ===
+
+let numberOne = 1
+console.log(1 + 1 - 1)
+
